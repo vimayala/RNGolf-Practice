0ec84fedeb148bd7bbb7905d8ef1747a34cc61da
Victoria Ayala <139914452+vimayala@users.noreply.github.com>
2024-02-02 18:36:24 -0800

    Ball reset, x position, and obstacle moves

--- src/Play.js
+++ src/Play.js
@@ -8,7 +8,7 @@ class Play extends Phaser.Scene {
         this.SHOT_VELOCITY_X = 200
         this.SHOT_VELOCITY_Y_MIN = 700
         this.SHOT_VELOCITY_Y_MAX = 1100
-
+        this.WALL_SPEED = 1
     }
 
     preload() {
@@ -36,40 +36,45 @@ class Play extends Phaser.Scene {
         this.ball.body.setCollideWorldBounds(true)
         this.ball.body.setBounce(0.5)
         this.ball.body.setDamping(true).setDrag(0.5)
+
         // add walls
-        // let wallA = this.physics.add.sprite(0, height / 4, 'wall')
-        // wallA.setX(Phaser.Math.Between(0 + wallA.width / 2, width - wallA.width / 2))
-        // wallA.body.setImmovable(true)
+        this.wallA = this.physics.add.sprite(0, height / 4, 'wall')
+        this.wallA.setCollideWorldBounds(true)
+        this.wallA.setX(Phaser.Math.Between(0 + this.wallA.width / 2, width - this.wallA.width / 2))
+        this.wallA.body.setImmovable(true)
 
-        // let wallB = this.physics.add.sprite(0, height / 2, 'wall')
-        // wallB.setX(Phaser.Math.Between(0 + wallB.width / 2, width - wallB.width / 2))
-        // wallB.body.setImmovable(true)
+        let wallB = this.physics.add.sprite(0, height / 2, 'wall')
+        wallB.setX(Phaser.Math.Between(0 + wallB.width / 2, width - wallB.width / 2))
+        wallB.body.setImmovable(true)
 
-        // // make a group (passed in as array) to make collider
-        // this.walls = this.add.group([wallA, wallB])
+        // make a group (passed in as array) to make collider
+        this.walls = this.add.group([this.wallA, wallB])
 
-        // // add one-way
-        // this.oneWay = this.physics.add.sprite(width / 2, height/ 4 * 3, 'oneway')
-        // this.oneWay.setX(Phaser.Math.Between(0 + this.oneWay.width / 2, width - this.oneWay.width / 2))
-        // this.oneWay.body.setImmovable(true)
-        // this.oneWay.body.checkCollision.down = false
+        // add one-way
+        this.oneWay = this.physics.add.sprite(width / 2, height/ 4 * 3, 'oneway')
+        this.oneWay.setX(Phaser.Math.Between(0 + this.oneWay.width / 2, width - this.oneWay.width / 2))
+        this.oneWay.body.setImmovable(true)
+        this.oneWay.body.checkCollision.down = false
 
         // add pointer input
         this.input.on('pointerdown', (pointer) => {
             let shotDirection = pointer.y <= this.ball.y ? 1 : -1
             // this.ball.body.setVelocityX(Phaser.Math.Between(-this.SHOT_VELOCITY_X, this.SHOT_VELOCITY_X))
-            // this.ball.body.setVelocityY(Phaser.Math.Between(this.SHOT_VELOCITY_Y_MIN, this.SHOT_VELOCITY_Y_MAX) * shotDirection)
-            this.ball.body.setVelocityX(pointer.x)
-            this.ball.body.setVelocityY(pointer.y * shotDirection)
+            let shotX = pointer.x <= this.ball.x ? 150 : -150
+            this.ball.body.setVelocityX(shotX)
+            this.ball.body.setVelocityY(Phaser.Math.Between(this.SHOT_VELOCITY_Y_MIN, this.SHOT_VELOCITY_Y_MAX) * shotDirection)
+
+            // this.ball.body.setVelocityY(pointer.y * shotDirection)
         })
 
         // cup/ball collision
         this.physics.add.collider(this.ball, this.cup, (ball, cup) => {      //ball and cup auto passed in, just need local names
             this.ball.alpha = 0
-            this.ball.body.setVelocityX = 0
-            this.ball.body.setVelocityY = 0
-            this.ball.setPosition(width / 2, height - height / 10)
-            this.ball.alpha = 1
+            this.scene.restart('playScene')
+            // this.ball.body.setVelocityX = 0
+            // this.ball.body.setVelocityY = 0
+            // this.ball.setPosition(width / 2, height - height / 10)
+            // this.ball.alpha = 1
             // this.ball.reset(width / 2, height - height / 10)
         })
 
@@ -81,14 +86,22 @@ class Play extends Phaser.Scene {
     }
 
     update() {
-        // this.wallA.setPositionX -= 10
+        
+        this.wallA.x += this.WALL_SPEED
+        if(this.wallA.x == width - this.wallA.width / 2){
+            this.WALL_SPEED *= -1
+        }
+        else if (this.wallA.x == 93 ){
+            this.WALL_SPEED *= -1
+            
+        }
     }
 }
 /*
 CODE CHALLENGE
 Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
-[ ] Add ball reset logic on successful shot
-[ ] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
-[ ] Make one obstacle move left/right and bounce against screen edges
+[X] Add ball reset logic on successful shot
+[X] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
+[X] Make one obstacle move left/right and bounce against screen edges
 [ ] Create and display shot counter, score, and successful shot percentage
 */
